a78a3032a405e6b29a599190e66c15b8225026cd
ganeshkousik <ganeshkousik313@gmail.com>
2021-05-16 15:24:42 +0530

    url

--- server/server.js
+++ server/server.js
@@ -10,13 +10,16 @@ const userRoute = require('./routes/api/auth');
 
 app.use(bodyParser.json());
 app.use(cors({origin: 'https://react-auth-3d77f.web.app'}));
-app.use(express.static(path.resolve(__dirname, 'build')));
 app.use('/auth',userRoute);
 
 //ROUTES
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
+const PORT = process.env.PORT || 5000;
 
 //Connect to DB
 mongoose.set('useCreateIndex', true);
@@ -26,4 +29,4 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTo
 
 exports.app = functions.https.onRequest(app);
 //Listen Server
-app.listen(5000);
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
